66f057115fa2fed5dff62d90207f4214f77ce7a5
Kevin Majesta <kevinivano1205@gmail.com>
2025-02-27 16:35:31 +0700

    fix

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/Kevinmajesta/Shortener-URL/database"
 	"github.com/Kevinmajesta/Shortener-URL/handlers"
 	"github.com/Kevinmajesta/Shortener-URL/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -15,6 +16,14 @@ func main() {
 	database.DB.AutoMigrate(&models.URL{})
 
 	r := gin.Default()
+
+	// Tambahkan middleware CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Ganti sesuai frontend Anda
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true,
+	}))
 
 	// Routes
 	r.POST("/shorten", handlers.CreateShortURL)
